fdcb8753fb8b5bab6bc139fb126aabfe81224eac
Gregory McIntyre <gregf50@gmail.com>
2017-08-25 14:57:30 +0200

    Initial Project commit

---
+++ MonitoringApplication/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonitoringApplication
+//
+//  Created by Gregory McIntyre on 2017/08/25.
+//  Copyright © 2017 Greg McIntyre. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
